aa19ae005dbca76ba7c58b3b174cce581bd6ee3a
yaneth <yanethcortezh@gmail.com>
2022-05-27 15:17:55 -0400

    second attend

--- src/test/java/TestGithub.java
+++ src/test/java/TestGithub.java
@@ -3,13 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class testGithub {
+public class TestGithub {
 
     @Test
     public void testGithub() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
+        driver.get("https://google.com");
 
     }
 }
