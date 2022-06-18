47778f8e2144879b5fbff78e049808adaa6d1e55
Yaneth Cortez <yanethcortezh@gmail.com>
2022-06-18 12:25:36 -0400

    newclass Test2

---
+++ src/test/java/NewClassTest2.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewClassTest2 {
+
+    @Test
+    public void company () {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://amazon.com");
+
+    }
+
+    @Test
+    public void spectreMethod() {
+        System.out.println("This is the Spectre branch");
+        Assert.assertEquals("spectre","spectre");
+
+    }
+
+}
